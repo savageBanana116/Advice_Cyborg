81ba2d5dd4aafedbc21f6ed9da4b5571fa9d447f
Cybertas <chris20419@gmail.com>
2022-08-09 19:03:46 +1000

    Car Lease calculator done, however interest is not valid

--- templates/Pages/car_lease_calculator.php
+++ templates/Pages/car_lease_calculator.php
@@ -9,15 +9,58 @@
 <style>
 
 </style>
-
+<link rel="stylesheet" href="\team18-app_fit3048\webroot\css\styles.css" type="text/css">
 <body>
 <div class="container" >
     <div class="text-center" style="margin-top: 5%">
         <h2 class="text-uppercase" style="font-size: 56px">Car Lease Calculators</h2>
-        <p class="text-muted">Calculate your___________</p>
+        <form id="lease_form" >
+            <ul class="no-bullets">
+                <li>
+                    <label for="Loan_Date">Date of Loan:</label>
+                    <input type="date" id="Loan_Date">
+                </li>
+                <li>
+                    <label for="Loan_Amount">Amount of Loan ($):</label>
+                    <input type="number" id="Loan_Amount" >
+                </li>
+                <li>
+                    <label for="Annual_Interest_Rate">Annual Interest Rate (%):</label>
+                    <input type="number" id="Annual_Interest_Rate">
+                </li>
+                <li>
+                    <label for="Terms_of_Loan">Terms of Loan (Years):</label>
+                    <input type="number" id="Terms_of_Loan">
+                </li>
+                <li>
+                    <label for="Number_of_Payment_Annually">Number of Payments Annually:</label>
+                    <input type="number" id="Number_of_Payment_Annually">
+                </li>
+            </ul>
+        </form>
+
+        <button id="Submit"> Calculate </button>
+
+        <form id="lease_form" >
+            <ul class="no-bullets">
+                <li>
+                    <label for="Loan_Date">Total Number of Payments:</label>
+                    <input type="text" id="Total_NO_Payments" readonly>
+                </li>
+                <li>
+                    <label for="Loan_Amount">Final Payment Date:</label>
+                    <input type="text" id="Final_Payment_Date" readonly>
+                </li>
+                <li>
+                    <label for="Annual_Interest_Rate">Total Interest:</label>
+                    <input type="text" id="Total_Interest" readonly>
+                </li>
+            </ul>
+        </form>
     </div>
 
 </div>
+<!--
 <nav class="navbar navbar-expand-lg navbar-dark bg-success py-4 w-100" style="width: 100%; margin-bottom: 25%">
 
     <div class="collapse navbar-collapse" id="navbarNav" style="font-size: 300%;color: white">
@@ -40,6 +83,9 @@
         </ul>
     </div>
 </nav>
+-->
 
 
+
+<script src="\team18-app_fit3048\webroot\js\Lease_calculator.js"></script>
 </body>

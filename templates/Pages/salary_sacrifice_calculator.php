67ec1dc1a36a0c7b79b7ee5329e309c9013df23a
Cybertas <chris20419@gmail.com>
2022-08-11 09:42:47 +1000

    salary sacrifice update

--- templates/Pages/salary_sacrifice_calculator.php
+++ templates/Pages/salary_sacrifice_calculator.php
@@ -13,12 +13,70 @@
 
 <body>
 <div class="container">
+    <!--
     <div class="text-center" style="margin-top: 5%">
         <h2 class="text-uppercase" style="font-size: 56px">Salary Sacrifice Calculators</h2>
         <p class="text-muted">Calculate your ___________</p>
+    </div>-->
+    <div class="container" >
+        <div class="text-center" style="margin-top: 5%">
+            <h2 class="text-uppercase" style="font-size: 56px">Salary Sacrifice Calculators</h2>
+            <form id="Salary_Sacrifice_Form" >
+                <ul class="no-bullets">
+                    <li>
+                        <label for="DOB">Date of Birth:</label>
+                        <input type="date" id="DOB">
+                    </li>
+                    <li>
+                        <label for="Salary">Salary ($):</label>
+                        <input type="number" id="Salary" >
+                    </li>
+                    <li>
+                        <label for="Salary_Sacrifice_Amount">Salary Sacrifice Amount :</label>
+                        <input type="number" id="Salary_Sacrifice_Amount">
+                    </li>
+                    <li>
+                        <label for="is_Family">Are you considered apart of a family  (Yes/No):</label>
+                        <input type="text" id="is_Family">
+                    </li>
+                </ul>
+            </form>
+
+            <button id="Submit"> Calculate </button>
+
+            <form id="Salary_Sacrifice_Form_Output" >
+                <ul class="no-bullets">
+                    <li>
+                        <label for="Finical_Year_Ending">Total Number of Payments:</label>
+                        <input type="text" id="Finical_Year_Ending" readonly>
+                    </li>
+                    <li>
+                        <label for="Client_Age">Final Payment Date:</label>
+                        <input type="text" id="Client_Age" readonly>
+                    </li>
+                    <li>
+                        <label for="Concessional_Contribution_Cap">Concessional Contribution Cap:</label>
+                        <input type="text" id="Concessional_Contribution_Cap" readonly>
+                    </li>
+                    <li>
+                        <label for="SG_Contributions">SG Contributions:</label>
+                        <input type="text" id="SG_Contributions" readonly>
+                    </li>
+                    <li>
+                        <label for="Allowed_Salary_Sacrifice">Salary Sacrifice Allowable :</label>
+                        <input type="text" id="Allowed_Salary_Sacrifice" readonly>
+                    </li>
+                </ul>
+            </form>
+        </div>
+
     </div>
 
+
+
+
 </div>
+<!--
 <nav class="navbar navbar-expand-lg navbar-dark bg-success py-4 w-100" style="width: 100%; margin-bottom: 25%">
 
     <div class="collapse navbar-collapse" id="navbarNav" style="font-size: 300%;color: white">
@@ -41,6 +99,6 @@
         </ul>
     </div>
 </nav>
-
+-->
 
 </body>

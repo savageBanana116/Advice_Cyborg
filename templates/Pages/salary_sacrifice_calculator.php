13c45e96a2c422a4fc660dddbf105b03b8c5c708
Cybertas <chris20419@gmail.com>
2022-08-13 13:17:17 +1000

    basic functionality added to Salary Sacrifice Calcualtor

--- templates/Pages/salary_sacrifice_calculator.php
+++ templates/Pages/salary_sacrifice_calculator.php
@@ -37,22 +37,22 @@
                     </li>
                     <li>
                         <label for="is_Family">Are you considered apart of a family  (Yes/No):</label>
-                        <input type="text" id="is_Family">
+                        <input type="checkbox" id="is_Family">
                     </li>
                 </ul>
             </form>
 
-            <button id="Submit"> Calculate </button>
-
+            <button id="Submit_Salary_Sacrifice"> Calculate </button>
+            <h3> Basic Information </h3>
             <form id="Salary_Sacrifice_Form_Output" >
                 <ul class="no-bullets">
                     <li>
-                        <label for="Finical_Year_Ending">Total Number of Payments:</label>
-                        <input type="text" id="Finical_Year_Ending" readonly>
+                        <label for="Age">Age :</label>
+                        <input type="text" id="Age" readonly>
                     </li>
                     <li>
-                        <label for="Client_Age">Final Payment Date:</label>
-                        <input type="text" id="Client_Age" readonly>
+                        <label for="Finical_Year_Ending">Total Number of Payments:</label>
+                        <input type="text" id="Finical_Year_Ending" readonly>
                     </li>
                     <li>
                         <label for="Concessional_Contribution_Cap">Concessional Contribution Cap:</label>
@@ -66,8 +66,152 @@
                         <label for="Allowed_Salary_Sacrifice">Salary Sacrifice Allowable :</label>
                         <input type="text" id="Allowed_Salary_Sacrifice" readonly>
                     </li>
+
+                    <h3>After Tax Contributions</h3>
+                    <li>
+                        <label for="Salary">Salary :</label>
+                        <input type="text" id="After_Tax_Salary" readonly>
+                    </li>
+                    <li>
+                        <label for="Salary_Sacrifice">Salary Sacrifice :</label>
+                        <input type="text" id="After_Tax_Salary_Sacrifice" readonly>
+                    </li>
+                    <li>
+                        <label for="Taxable_Income">Taxable Income:</label>
+                        <input type="text" id="After_Tax_Taxable_Income" readonly>
+                    </li>
+                    <li>
+                        <label for="Tax_Payable">Basic Tax Payable :</label>
+                        <input type="text" id="After_Tax_Payable_Basic" readonly>
+                    </li>
+                    <li>
+                        <label for="Levy"> Medicare Levy :</label>
+                        <input type="text" id="After_Levy" readonly>
+                    </li>
+                    <li>
+                        <label for="LM_Tax_Offset">Low & Middle Income Tax Offset :</label>
+                        <input type="text" id="After_LM_Tax_Offset" readonly>
+                    </li>
+                    <li>
+                        <label for="Low_Income_Tax_Offset">Low Income Tax Offset :</label>
+                        <input type="text" id="After_Low_Income_Tax_Offset" readonly>
+                    </li>
+                    <li>
+                        <label for="Tax_Payable">Tax Payable (Including ML) :</label>
+                        <input type="text" id="After_Tax_Payable" readonly>
+                    </li>
+                    <li>
+                        <label for="Net_Income">Net Income :</label>
+                        <input type="text" id="After_Net_Income" readonly>
+                    </li>
+                    <li>
+                        <label for="Less_After_Tax">Less After Tax Contributions :</label>
+                        <input type="text" id="After_Less_After_Tax" readonly>
+                    </li>
+                    <li>
+                        <label for="Take_Home_Pay">Take Home Pay :</label>
+                        <input type="text" id="After_Take_Home_Pay" readonly>
+                    </li>
+                    <h5> Effects on your Super Contributions - After Tax </h5>
+                    <li>
+                        <label for="After_SG">10% SG ($) :</label>
+                        <input type="text" id="After_SG" readonly>
+                    </li>
+                    <li>
+                        <label for="After_Salary_Sacrifice">Salary Sacrifice ($) :</label>
+                        <input type="text" id="After_Salary_Sacrifice" readonly>
+                    </li>
+                    <li>
+                        <label for="After_Less_Contribution_Tax">Less 15% Contributions Tax :</label>
+                        <input type="text" id="After_Less_Contribution_Tax" readonly>
+                    </li>
+                    <li>
+                        <label for="After_tax">After Tax ($) :</label>
+                        <input type="text" id="After_tax" readonly>
+                    </li>
+
+                    <h3>Pre-Tax Contributions</h3>
+                    <li>
+                        <label for="Salary">Salary :</label>
+                        <input type="text" id="Pre_Tax_Salary" readonly>
+                    </li>
+                    <li>
+                        <label for="Salary_Sacrifice">Salary Sacrifice :</label>
+                        <input type="text" id="Pre_Tax_Salary_Sacrifice" readonly>
+                    </li>
+                    <li>
+                        <label for="Taxable_Income">Taxable Income:</label>
+                        <input type="text" id="Pre_Tax_Taxable_Income" readonly>
+                    </li>
+                    <li>
+                        <label for="Tax_Payable">Basic Tax Payable :</label>
+                        <input type="text" id="Pre_Tax_Payable_Basic" readonly>
+                    </li>
+                    <li>
+                        <label for="Levy"> Medicare Levy :</label>
+                        <input type="text" id="Pre_Levy" readonly>
+                    </li>
+                    <li>
+                        <label for="LM_Tax_Offset">Low & Middle Income Tax Offset :</label>
+                        <input type="text" id="Pre_LM_Tax_Offset" readonly>
+                    </li>
+                    <li>
+                        <label for="Low_Income_Tax_Offset">Low Income Tax Offset :</label>
+                        <input type="text" id="Pre_Low_Income_Tax_Offset" readonly>
+                    </li>
+                    <li>
+                        <label for="Tax_Payable">Tax Payable (Including ML) :</label>
+                        <input type="text" id="Pre_Tax_Payable" readonly>
+                    </li>
+                    <li>
+                        <label for="Net_Income">Net Income :</label>
+                        <input type="text" id="Pre_Net_Income" readonly>
+                    </li>
+                    <li>
+                        <label for="Less_After_Tax">Less After Tax Contributions :</label>
+                        <input type="text" id="Pre_Less_After_Tax" readonly>
+                    </li>
+                    <li>
+                        <label for="Take_Home_Pay">Take Home Pay :</label>
+                        <input type="text" id="Pre_Take_Home_Pay" readonly>
+                    </li>
+                    <h5> Effects on your Super Contributions - Pre-Tax </h5>
+                    <li>
+                        <label for="Pre_SG">10% SG ($) :</label>
+                        <input type="text" id="Pre_SG" readonly>
+                    </li>
+                    <li>
+                        <label for="Pre_Salary_Sacrifice">Salary Sacrifice ($) :</label>
+                        <input type="text" id="Pre_Salary_Sacrifice" readonly>
+                    </li>
+                    <li>
+                        <label for="Pre_Less_Contribution_Tax">Less 15% Contributions Tax :</label>
+                        <input type="text" id="Pre_Less_Contribution_Tax" readonly>
+                    </li>
+                    <li>
+                        <label for="Pre_After_tax">After Tax ($) :</label>
+                        <input type="text" id="Pre_After_tax" readonly>
+                    </li>
+                    <h3> Summary </h3>
+                    <li>
+                        <label for="Tax_Saving">Personal Tax Saving :</label>
+                        <input type="text" id="Tax_Saving" readonly>
+                    </li>
+                    <li>
+                        <label for="Total_TaxSaving">Total Tax Saving :</label>
+                        <input type="text" id="Total_TaxSaving" readonly>
+                    </li>
+                    <li>
+                        <label for="Increase">Increase In Take Home Pay and Super :</label>
+                        <input type="text" id="Increase" readonly>
+                    </li>
                 </ul>
             </form>
+
+
+
+
+
         </div>
 
     </div>
@@ -100,5 +244,5 @@
     </div>
 </nav>
 -->
-
+<script src = "\team18-app_fit3048\webroot\js\salary_sacrifice.js"></script>
 </body>
---
+++ webroot/js/salary_sacrifice.js
@@ -0,0 +1,202 @@
+
+const btn = document.getElementById('Submit_Salary_Sacrifice');
+btn.addEventListener('click',getSSInput);
+
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+function getConcessionalCap(Age){
+    if(Age >= 75){
+        return 0;
+    }
+    else if (Age >= 50){
+        return 27500;
+    }
+    return 27500;
+
+}
+
+function getTaxPayable(salary){
+
+        const Taxable_Income = salary;
+        if(Taxable_Income <= 18200){
+            return 0;
+        } else if(Taxable_Income <=45000){
+            return (Taxable_Income-18200)*19/100;
+        } else if(Taxable_Income <= 120000){
+            return (Taxable_Income - 45000) * 32.5/100 + 5092;
+        } else if (Taxable_Income <= 180000){
+            return (Taxable_Income - 120000) * 37/100 + 29467;
+        } else if (Taxable_Income > 180000){
+            return (Taxable_Income - 180000) * 45/100 + 51667;
+        }
+
+
+
+}
+
+function getLevy(salary,is_Family){
+
+    if (is_Family == false){
+        const Taxable_Income = salary;
+        const total = Taxable_Income * 2 / 100;
+
+        if(Taxable_Income <= 90000){
+            return total;
+        }else if(Taxable_Income <= 105000){
+            return total + (Taxable_Income * 1 / 100);
+        }else if(Taxable_Income <= 140000){
+            return total + (Taxable_Income * 1.25 / 100);
+        }else if(Taxable_Income > 140000){
+            return total + (Taxable_Income * 1.5 / 100);
+        }
+
+    }else if (is_Family == true){
+        const Taxable_Income = salary;
+        const total = Taxable_Income * 2 / 100;
+        if(Taxable_Income <= 180000){
+            return total;
+        }else if(Taxable_Income <= 210000){
+            return total + (Taxable_Income * 1 / 100);
+        }else if(Taxable_Income <= 280000){
+            return total + (Taxable_Income * 1.25 / 100);
+        }else if(Taxable_Income > 280000){
+            return total + (Taxable_Income * 1.5 /100);
+        }
+    }
+}
+
+function getLMTaxOffset(salary){
+
+
+        const Taxable_Income = salary;
+        if(Taxable_Income <= 37000){
+            return 255;
+        }else if(Taxable_Income <= 48000){
+            return 255 + (0.075 * (Taxable_Income-37000));
+        }else if(Taxable_Income <= 90000){
+            return 1080;
+        }else if(Taxable_Income <= 126000){
+            return 1080 - (0.03 * (Taxable_Income - 90000));
+        } else{
+            return 0;
+        }
+
+}
+
+function getLowIncomeTaxOffset(salary){
+
+
+        const Taxable_Income = salary;
+        if(Taxable_Income <= 37500){
+            return 700;
+        }else if(Taxable_Income <= 45000){
+            return 700 - (0.05 * (Taxable_Income - 37500));
+        }else if(Taxable_Income <= 66667){
+            return 325 - (0.015 * (Taxable_Income - 45000));
+        }else {
+            return 0;
+        }
+
+}
+function getAge(DOB) {
+    const Age = new Date(Date.parse(DOB));
+    const Current_date = new Date();
+    const C_Age = Current_date.getFullYear() - Age.getFullYear();
+
+    return C_Age;
+}
+
+
+function getSSInput() {
+    const DOB = document.getElementById("DOB").value;
+    const Salary = document.getElementById("Salary").value;
+    const Salary_Sacrifice_Amount = document.getElementById("Salary_Sacrifice_Amount").value;
+    const is_Family = document.getElementById("is_Family").checked;
+
+    const DOB_String = DOB.toString();
+    const Age = getAge(DOB_String);
+    const Current_date = new Date();
+    const ConcessionalCap = getConcessionalCap(Age);
+    const SGContributions = 0.1 * Salary;
+    const Sacrifice_Allowable = ConcessionalCap - SGContributions;
+
+    //Variables for After Tax
+    const After_Basic_Tax_Payable = getTaxPayable(Salary);
+    const After_Levy = getLevy(Salary,is_Family);
+    const After_LMTaxOffset = getLMTaxOffset(Salary);
+    const After_LIncomeTaxOffset = getLowIncomeTaxOffset(Salary);
+    const After_Tax_Payable = After_Basic_Tax_Payable + After_Levy - After_LMTaxOffset - After_LIncomeTaxOffset;
+    const After_Net_Income = Salary - After_Tax_Payable;
+
+    //Variables for Pre Tax
+    const Pre_Taxable_Income = Salary - Salary_Sacrifice_Amount;
+    const Pre_Basic_Tax_Payable = getTaxPayable(Pre_Taxable_Income);
+    const Pre_Levy = getLevy(Pre_Taxable_Income,is_Family);
+    const Pre_LMTaxOffset = getLMTaxOffset(Pre_Taxable_Income);
+    const Pre_LIncomeTaxOffset = getLowIncomeTaxOffset(Pre_Taxable_Income);
+    const Pre_Tax_Payable = Pre_Basic_Tax_Payable + Pre_Levy - Pre_LMTaxOffset - Pre_LIncomeTaxOffset;
+    const Pre_Net_Income = Pre_Taxable_Income - Pre_Tax_Payable;
+
+    //output 1 - from 'step 1'
+    document.getElementById("Finical_Year_Ending").value = "30/June " + Current_date.getFullYear();
+    document.getElementById("Age").value = Age;
+    document.getElementById("Concessional_Contribution_Cap").value = ConcessionalCap;
+    document.getElementById("SG_Contributions").value = SGContributions;
+    document.getElementById("Allowed_Salary_Sacrifice").value = Sacrifice_Allowable;
+
+
+    //output 2  (After Tax) - from 'results'
+    document.getElementById("After_Tax_Salary").value = Salary;
+    document.getElementById("After_Tax_Salary_Sacrifice").value = 0;
+    document.getElementById("After_Tax_Taxable_Income").value = Salary;
+    document.getElementById("After_Tax_Payable_Basic").value = After_Basic_Tax_Payable;
+    document.getElementById("After_Levy").value = After_Levy;
+    document.getElementById("After_LM_Tax_Offset").value = After_LMTaxOffset;
+    document.getElementById("After_Low_Income_Tax_Offset").value = After_LIncomeTaxOffset;
+    document.getElementById("After_Tax_Payable").value = After_Tax_Payable;
+    document.getElementById("After_Net_Income").value = After_Net_Income;
+    document.getElementById("After_Less_After_Tax").value = 0;
+    document.getElementById("After_Take_Home_Pay").value = After_Net_Income - 0;
+
+    //output 3 for effect super contributions - After tax
+    const After_Less_Contribution_Tax = (SGContributions) * 0.15;
+    console.log(SGContributions, Salary_Sacrifice_Amount,0.15);
+    document.getElementById("After_SG").value = SGContributions;
+    document.getElementById("After_Salary_Sacrifice").value = Salary_Sacrifice_Amount;
+    document.getElementById("After_Less_Contribution_Tax").value = After_Less_Contribution_Tax;
+    document.getElementById("After_tax").value = 0;
+
+
+    //output 4 (Pre Tax) - from 'results'
+    document.getElementById("Pre_Tax_Salary").value = Salary;
+    document.getElementById("Pre_Tax_Salary_Sacrifice").value = Salary_Sacrifice_Amount;
+    document.getElementById("Pre_Tax_Taxable_Income").value = Pre_Taxable_Income;
+    document.getElementById("Pre_Tax_Payable_Basic").value = Pre_Basic_Tax_Payable;
+    document.getElementById("Pre_Levy").value = Pre_Levy;
+    document.getElementById("Pre_LM_Tax_Offset").value = Pre_LMTaxOffset;
+    document.getElementById("Pre_Low_Income_Tax_Offset").value = Pre_LIncomeTaxOffset;
+    document.getElementById("Pre_Tax_Payable").value = Pre_Tax_Payable;
+    document.getElementById("Pre_Net_Income").value = Pre_Net_Income;
+    document.getElementById("Pre_Less_After_Tax").value = 0;
+    document.getElementById("Pre_Take_Home_Pay").value = Pre_Net_Income - 0;
+
+    //output 5 for effect super contributions - Pre tax
+    const Less_Contribution_Tax = (SGContributions +  Number(Salary_Sacrifice_Amount)) * 15 / 100;
+    document.getElementById("Pre_SG").value = SGContributions;
+    document.getElementById("Pre_Salary_Sacrifice").value = 0;
+    document.getElementById("Pre_Less_Contribution_Tax").value = Less_Contribution_Tax;
+    document.getElementById("Pre_After_tax").value = 0;
+
+    //output 6 - summary
+    const Tax_Saving = After_Net_Income - Pre_Net_Income;
+    const Total_Tax_Saving = Tax_Saving - (Number(Salary_Sacrifice_Amount) * 0.15)
+    const Increase_A = Pre_Net_Income + SGContributions + Number(Salary_Sacrifice_Amount) - Less_Contribution_Tax
+    const Increase_B = After_Net_Income + SGContributions - After_Less_Contribution_Tax
+    const Increase = Increase_A - Increase_B;
+    console.log(Increase_A,Increase_B);
+    document.getElementById("Tax_Saving").value = Tax_Saving;
+    document.getElementById("Total_TaxSaving").value = Total_Tax_Saving;
+    document.getElementById("Increase").value = Increase;
+
+
+}

90480426126a73c7de2e2856b11b2f4e5faf71b4
Khargan <kpow0010@student.monash.edu>
2022-08-14 16:38:37 +1000

    Initial Implementation of Front-End for Car Lease Calculator

--- templates/Pages/car_lease_calculator.php
+++ templates/Pages/car_lease_calculator.php
@@ -13,53 +13,93 @@
 <body>
 <div class="container" >
     <div class="text-center" style="margin-top: 5%">
-        <h2 class="text-uppercase" style="font-size: 56px">Car Lease Calculators</h2>
-        <form id="lease_form" >
-            <ul class="no-bullets">
-                <li>
-                    <label for="Loan_Date">Date of Loan:</label>
-                    <input type="date" id="Loan_Date">
-                </li>
-                <li>
-                    <label for="Loan_Amount">Amount of Loan ($):</label>
-                    <input type="number" id="Loan_Amount" >
-                </li>
-                <li>
-                    <label for="Annual_Interest_Rate">Annual Interest Rate (%):</label>
-                    <input type="number" id="Annual_Interest_Rate">
-                </li>
-                <li>
-                    <label for="Terms_of_Loan">Terms of Loan (Years):</label>
-                    <input type="number" id="Terms_of_Loan">
-                </li>
-                <li>
-                    <label for="Number_of_Payment_Annually">Number of Payments Annually:</label>
-                    <input type="number" id="Number_of_Payment_Annually">
-                </li>
-            </ul>
+        <h1 >Car Lease Calculators</h1>
+        <div class="row">
+            <div class="col" style="border-style:solid; border-color:black; background-color: #403535; color: white;"><h3> To Get Started!</h3>
+            <br>
+            <h5>Tell us about your Loan, we will use this to calculate the amount of the loan as well as the final payment date.</h5></div>
+            <div class="col" style="margin-left: 5%; border-style:solid; border-color:black; background-color: #403535; color: white;">
+                <h5>Your Expected Loan Interest</h5></div>
+        </div>
+<!--      -->
+        <div class="row">
+            <div class="col" style="border-style:solid; border-color:black;" >
+        <form id="lease_form">
+            <div class="form-group">
+                <label for="Loan_Date">Date Of Loan</label>
+                <input id="Loan_Date" name="Loan_date" placeholder="dd/mm/yyyy" type="date" class="form-control" required="required">
+            </div>
+            <div class="form-group">
+                <label for="Loan_Amount">Amount Of Loan $</label>
+                <input id="Loan_Amount" name="Loan_Amount" placeholder="30000" type="number" class="form-control" required="required">
+            </div>
+            <div class="form-group">
+                <label for="Annual_Interest_Rate">Annual Interest Rate (%)</label>
+                <input id="Annual_Interest_Rate" name="Annual_Interest_Rate" placeholder="5" type="number" class="form-control" required="required">
+            </div>
+            <div class="form-group">
+                <label for="Terms_of_Loan">Term Of Loan (Years)</label>
+                <input id="Terms_of_Loan" name="Terms_of_Loan" placeholder="3" type="number" class="form-control" required="required">
+            </div>
+            <div class="form-group">
+                <label for="Number_of_Payment_Annually">Number of Payments Annually</label>
+                <input id="Number_of_Payment_Annually" name="Number_of_Payment_Annually" placeholder="32" type="number" class="form-control" required="required">
+            </div>
         </form>
+<br>
+        <div class="form-group">
+            <button id="Submit" class="btn btn-primary">Calculate</button>
+        </div>
+                <br>
+            </div>
 
-        <button id="Submit"> Calculate </button>
 
+<!--        <form id="lease_form" >-->
+<!--            <ul class="no-bullets">-->
+<!--                <li>-->
+<!--                    <label for="Loan_Date">Date of Loan:</label>-->
+<!--                    <input type="date" id="Loan_Date">-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    <label for="Loan_Amount">Amount of Loan ($):</label>-->
+<!--                    <input type="number" id="Loan_Amount" >-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    <label for="Annual_Interest_Rate">Annual Interest Rate (%):</label>-->
+<!--                    <input type="number" id="Annual_Interest_Rate">-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    <label for="Terms_of_Loan">Terms of Loan (Years):</label>-->
+<!--                    <input type="number" id="Terms_of_Loan">-->
+<!--                </li>-->
+<!--                <li>-->
+<!--                    <label for="Number_of_Payment_Annually">Number of Payments Annually:</label>-->
+<!--                    <input type="number" id="Number_of_Payment_Annually">-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--        </form>-->
+
+<!--        <button id="Submit"> Calculate </button>-->
+            <div class="col" style="margin-left: 5%; border-style:solid; border-color:black;">
         <form id="lease_form" >
-            <ul class="no-bullets">
-                <li>
+            <div class="form-group">
                     <label for="Loan_Date">Total Number of Payments:</label>
-                    <input type="text" id="Total_NO_Payments" readonly>
-                </li>
-                <li>
+                    <output id="Total_NO_Payments" readonly></output>
+            </div>
+            <div class="form-group">
                     <label for="Loan_Amount">Final Payment Date:</label>
-                    <input type="text" id="Final_Payment_Date" readonly>
-                </li>
-                <li>
+                    <output type="text" id="Final_Payment_Date" readonly></output>
+            </div>
+            <div class="form-group">
                     <label for="Annual_Interest_Rate">Total Interest:</label>
-                    <input type="text" id="Total_Interest" readonly>
-                </li>
-            </ul>
+                    <output type="text" id="Total_Interest" readonly></output>
+            </div>
         </form>
     </div>
 
 </div>
+    </div>
+
 <!--
 <nav class="navbar navbar-expand-lg navbar-dark bg-success py-4 w-100" style="width: 100%; margin-bottom: 25%">
 

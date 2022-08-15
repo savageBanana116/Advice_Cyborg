f3407b805ea8e05e23900005dbb3952b1cbfd5f4
Cybertas <chris20419@gmail.com>
2022-08-15 20:52:19 +1000

    lease calculator rounded to 2 decimal places

--- webroot/js/Lease_calculator.js
+++ webroot/js/Lease_calculator.js
@@ -1,7 +1,8 @@
 
 const btn = document.getElementById('Submit');
 btn.addEventListener('click',getInput);
-function getInput() {
+function getInput()
+{
     const Loan_Date = document.getElementById("Loan_Date").value;
     const Loan_Amount = document.getElementById("Loan_Amount").value;
     const Interest_Rate = document.getElementById("Annual_Interest_Rate").value;
@@ -16,14 +17,11 @@ function getInput() {
     Final_Payment_Date.setFullYear(parseInt(Loan_Term) + Final_Payment_Date.getFullYear())
     const Total_NO_Payments = Loan_Term * No_Payment;
     const Total_Interest = Loan_Amount * Interest_Rate_Percent * Loan_Term;
+    const Interest = Math.round(Total_Interest * 100) / 100;
 
-
-    console.log("Total Number of Payments is: " + Total_NO_Payments);
-    console.log("Final Payment Date is: " + Final_Payment_Date.toDateString());
-    console.log("Total Interest is: " + Total_Interest);
-
+    console.log(Interest);
     document.getElementById("Total_NO_Payments").value = Total_NO_Payments;
     document.getElementById("Final_Payment_Date").value = Final_Payment_Date.toDateString();
-    document.getElementById("Total_Interest").value = Total_Interest;
+    document.getElementById("Total_Interest").value = Interest;
 
 }

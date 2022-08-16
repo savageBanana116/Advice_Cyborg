00736b1da3b4f6670d2f6857dc0c4d055101879e
Khargan <kpow0010@student.monash.edu>
2022-08-16 13:44:55 +1000

    CSS For Car Lease Calculator and Retirement Calculator

--- templates/Pages/car_lease_calculator.php
+++ templates/Pages/car_lease_calculator.php
@@ -11,6 +11,30 @@
 </style>
 <link rel="stylesheet" href="\team18-app_fit3048\webroot\css\styles.css" type="text/css">
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-success py-4 w-100"  ">
+
+<div class="collapse navbar-collapse" id="navbarNav" >
+    <ul class="navbar-nav">
+        <li class="nav-item active px-5" style="border-bottom:  5px solid #f5f4eb">
+            <a class="nav-link" href="car_lease_calculator">Car Lease <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item px-5 ">
+            <a class="nav-link" href="income_tax_calculator">Income Tax</a>
+        </li>
+        <li class="nav-item px-5">
+            <a class="nav-link" href="retirement_calculator">Retirement</a>
+        </li>
+        <li class="nav-item px-5">
+            <a class="nav-link" href="salary_sacrifice_calculator">Salary Sacrifice</a>
+        </li>
+        <li class="nav-item px-3">
+            <a class="nav-link" href="age_pension_calculator.php">Age Pension</a>
+        </li>
+    </ul>
+</div>
+</nav>
+
 <div class="container" >
     <div class="text-center" style="margin-top: 5%">
         <h1 >Car Lease Calculators</h1>
@@ -24,27 +48,44 @@
 <!--      -->
         <div class="row">
             <div class="col" style="border-style:solid; border-color:black;" >
+
         <form id="lease_form">
-            <div class="form-group">
-                <label for="Loan_Date">Date Of Loan</label>
+            <br>
+            <div class="form-group row">
+                <label for="Loan_Date" class="col-4 col-form-label">Date Of Loan</label>
+                <div class="col-8">
                 <input id="Loan_Date" name="Loan_date" placeholder="dd/mm/yyyy" type="date" class="form-control"  required="required">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="Loan_Amount">Amount Of Loan $</label>
+            <br>
+            <div class="form-group row">
+                <label for="Loan_Amount" class="col-4 col-form-label">Amount Of Loan $</label>
+                <div class="col-8">
                 <input id="Loan_Amount" name="Loan_Amount" placeholder="30000" type="number" class="form-control" min="1" max="999999999" required="required">
+                </div>
             </div>
-            <div class="form-group">
-                <label for="Annual_Interest_Rate">Annual Interest Rate (%)</label>
+            <br>
+            <div class="form-group row">
+                <label for="Annual_Interest_Rate" class="col-4 col-form-label">Annual Interest Rate (%)</label>
+                <div class="col-8">
                 <input id="Annual_Interest_Rate" onKeyPress="return check(event,value)" onInput="checkLength()" name="Annual_Interest_Rate" placeholder="5" type="number" min="0" max="100" class="form-control" required="required">
-            </div>
-            <div class="form-group">
-                <label for="Terms_of_Loan">Term Of Loan (Years)</label>
+                </div>
+                </div>
+            <br>
+            <div class="form-group row">
+                <label for="Terms_of_Loan" class="col-4 col-form-label">Term Of Loan (Years)</label>
+                <div class="col-8">
                 <input id="Terms_of_Loan" name="Terms_of_Loan" placeholder="3" type="number" class="form-control" min="1" max="50" required="required">
             </div>
-            <div class="form-group">
-                <label for="Number_of_Payment_Annually">Number of Payments Annually</label>
+            </div>
+            <br>
+            <div class="form-group row">
+                <label for="Number_of_Payment_Annually" class="col-4 col-form-label">Number of Payments Annually</label>
+                <div class="col-8">
                 <input id="Number_of_Payment_Annually" name="Number_of_Payment_Annually" placeholder="32" type="number"  min="1" max="365" class="form-control" required="required">
             </div>
+            </div>
+            <br>
         </form>
 <br>
         <div class="form-group">
@@ -82,17 +123,23 @@
 <!--        <button id="Submit"> Calculate </button>-->
             <div class="col" style="margin-left: 5%; border-style:solid; border-color:black;">
         <form id="lease_form" >
-            <div class="form-group">
-                    <label for="Loan_Date">Total Number of Payments:</label>
+            <div class="form-group row" style="">
+                    <label for="Loan_Date" class="col-4 col-form-label">Total Number of Payments:</label>
+                <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
                     <output id="Total_NO_Payments" readonly></output>
+                </div>
             </div>
-            <div class="form-group">
-                    <label for="Loan_Amount">Final Payment Date:</label>
+            <div class="form-group row">
+                    <label for="Loan_Amount" class="col-4 col-form-label">Final Payment Date:</label>
+                <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
                     <output type="text" id="Final_Payment_Date" readonly></output>
+                </div>
             </div>
-            <div class="form-group">
-                    <label for="Annual_Interest_Rate">Total Interest:</label>
+            <div class="form-group row">
+                    <label for="Annual_Interest_Rate" class="col-4 col-form-label">Total Interest:</label>
+                <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
                     <output type="text" id="Total_Interest" readonly></output>
+                </div>
             </div>
         </form>
     </div>
--- templates/Pages/retirement_calculator.php
+++ templates/Pages/retirement_calculator.php
@@ -12,6 +12,28 @@
 </style>
 
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success py-4 w-100"  ">
+
+<div class="collapse navbar-collapse" id="navbarNav" >
+    <ul class="navbar-nav">
+        <li class="nav-item active px-5">
+            <a class="nav-link" href="car_lease_calculator">Car Lease <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item px-5 ">
+            <a class="nav-link" href="income_tax_calculator">Income Tax</a>
+        </li>
+        <li class="nav-item px-5" style="border-bottom:  5px solid #f5f4eb">
+            <a class="nav-link" href="retirement_calculator">Retirement</a>
+        </li>
+        <li class="nav-item px-5">
+            <a class="nav-link" href="salary_sacrifice_calculator">Salary Sacrifice</a>
+        </li>
+        <li class="nav-item px-3">
+            <a class="nav-link" href="age_pension_calculator.php">Age Pension</a>
+        </li>
+    </ul>
+</div>
+</nav>
 <div class="container">
     <!--
     <div class="text-center" style="margin-top: 5%">
@@ -21,82 +43,143 @@
     <div class="container" >
         <div class="text-center" style="margin-top: 5%">
             <h2 class="text-uppercase" style="font-size: 56px">Retirement Calculators</h2>
+            <div class="row">
+                <div class="col" style="border-style:solid; border-color:black; background-color: #403535; color: white;"><h3> To Get Started!</h3>
+                    <br>
+                    <h5>Tell us about your plans for retirement, we will use this to calculate the amount of the loan as well as the final payment date.</h5></div>
+                <div class="col" style="margin-left: 5%; border-style:solid; border-color:black; background-color: #403535; color: white;">
+                    <h5>Your Expected Loan Interest</h5></div>
+            </div>
+            <div class="row">
+                <div class="col" style="border-style:solid; border-color:black;" >
             <form id="Retirement_Calculator_Form" >
-                <ul class="no-bullets">
-                    <li>
-                        <label for="Age">Age: </label>
-                        <input type="number" id="Age">
-                    </li>
-                    <li>
-                        <label for="Retirement_Age">Retirement Age:</label>
-                        <input type="number" id="Retirement_Age" >
-                    </li>
-                    <li>
-                        <label for="Year_Payments">Year of Payments :</label>
-                        <input type="number" id="Year_Payments">
-                    </li>
-                    <li>
-                        <label for="Payment_Required_Today">Payment Required Amount Today:</label>
-                        <input type="text" id="Payment_Required_Today">
-                    </li>
-                    <li>
-                        <label for="Indexation">Indexation: </label>
-                        <input type="number" id="Indexation">
-                    </li>
-                    <li>
-                        <label for="Interest_Rate">Interest Rate: </label>
-                        <input type="number" id="Interest_Rate">
-                    </li>
-                    <li>
-                        <label for="Current_Super">Current Superannuation: </label>
-                        <input type="number" id="Current_Super">
-                    </li>
-                    <li>
-                        <label for="Current_Net_Contributions">Current Net Contributions: </label>
-                        <input type="number" id="Current_Net_Contributions">
-                    </li>
-                    <li>
-                        <label for="Estimated_Return_Rate">Current Rate of Return: </label>
-                        <input type="number" id="Estimated_Return_Rate">
-                    </li>
-                </ul>
+                <br>
+                <div class="form-group row">
+                        <label for="Age" class="col-4 col-form-label">Age: </label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Age">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Retirement_Age" class="col-4 col-form-label">Retirement Age:</label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Retirement_Age" >
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Year_Payments" class="col-4 col-form-label">Year of Payments :</label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Year_Payments">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Payment_Required_Today" class="col-4 col-form-label">Payment Required Amount Today:</label>
+                    <div class="col-8">
+                        <input type="text"  class="form-control" id="Payment_Required_Today">
+                    </div>
+                </div>
+                <br>
+
+                <div class="form-group row">
+                        <label for="Indexation" class="col-4 col-form-label">Indexation: </label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Indexation">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Interest_Rate" class="col-4 col-form-label">Interest Rate: </label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Interest_Rate">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Current_Super" class="col-4 col-form-label">Current Superannuation: </label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Current_Super">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Current_Net_Contributions" class="col-4 col-form-label">Current Net Contributions: </label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Current_Net_Contributions">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Estimated_Return_Rate" class="col-4 col-form-label">Current Rate of Return: </label>
+                    <div class="col-8">
+                        <input type="number" class="form-control" id="Estimated_Return_Rate">
+                    </div>
+                </div>
             </form>
+                    <br>
+                    <div class="form-group">
+            <button id="Submit_retirement" class="btn btn-primary" > Calculate </button>
+                    </div>
+                    <br>
+                </div>
 
-            <button id="Submit_retirement"> Calculate </button>
-
+                <div class="col" style="margin-left: 5%; border-style:solid; border-color:black;">
             <form id="Retirement_Calculator_Form_Output" >
-                <ul class="no-bullets">
-                    <li>
-                        <label for="Payment_Required_Future">Payment Required Amount Future :</label>
-                        <input type="text" id="Payment_Required_Future" readonly>
-                    </li>
-                    <li>
-                        <label for="Value_Required">Value Required :</label>
-                        <input type="text" id="Value_Required" readonly>
-                    </li>
-                    <li>
-                        <label for="Estimated_Annual_Deduct">Estimated Annual Deductible Amount:</label>
-                        <input type="text" id="Estimated_Annual_Deduct" readonly>
-                    </li>
-                    <li>
-                        <label for="Estimated_Super">Estimated Value of Super at Retirement:</label>
-                        <input type="text" id="Estimated_Super" readonly>
-                    </li>
-                    <li>
-                        <label for="Lumpsum">Lumpsum Shortfall:</label>
-                        <input type="text" id="Lumpsum" readonly>
-                    </li>
-                    <li>
-                        <label for="Net_Payment">Net Payment Required to Reach Target(Surplus):</label>
-                        <input type="text" id="Net_Payment" readonly>
-                    </li>
-                    <li>
-                        <label for="Gross_Payment">Gross Payment (Inc Surcharge):</label>
-                        <input type="text" id="Gross_Payment" readonly>
-                    </li>
-                </ul>
+                <br>
+                <div class="form-group row" s>
+                        <label for="Payment_Required_Future" class="col-4 col-form-label">Payment Required Amount Future :</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
+                        <output type="text" id="Payment_Required_Future" readonly></output>
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row" >
+                        <label for="Value_Required" class="col-4 col-form-label">Value Required :</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%6">
+                        <output type="text" id="Value_Required" readonly></output>
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row" >
+                        <label for="Estimated_Annual_Deduct" class="col-4 col-form-label">Estimated Annual Deductible Amount:</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
+                        <output type="text" id="Estimated_Annual_Deduct" readonly></output>
+                </div>
+                </div>
+                <br>
+                <div class="form-group row" >
+                        <label for="Estimated_Super" class="col-4 col-form-label">Estimated Value of Super at Retirement:</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
+                        <output type="text" id="Estimated_Super" readonly></output>
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row" >
+                        <label for="Lumpsum" class="col-4 col-form-label" >Lumpsum Shortfall:</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
+                        <output type="text" id="Lumpsum" readonly></output>
+                    </div>
+                </div>
+                <br>
+                <div class="form-group row">
+                        <label for="Net_Payment" class="col-4 col-form-label">Net Payment Required to Reach Target(Surplus):</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 10%">
+                        <output type="text" id="Net_Payment" readonly></output>
+                </div>
+                    <br>
+                </div>
+                <div class="form-group row" >
+                        <label for="Gross_Payment" class="col-4 col-form-label">Gross Payment (Inc Surcharge):</label>
+                    <div class="col-8" style="border-bottom: 0.5px solid black; padding-top: 5%">
+                        <output type="text" id="Gross_Payment" readonly></output>
+                    </div>
+                </div>
+                <br>
             </form>
         </div>
+            </div>
 
     </div>
 

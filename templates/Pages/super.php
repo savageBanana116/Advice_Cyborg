315a8ca04074bd21077729f785cb659a70bc7354
mliu0044 <mliu0044@student.monash.edu>
2022-08-22 11:57:21 +1000

    Change super page database connection for cPanel  version2

--- templates/Pages/super.php
+++ templates/Pages/super.php
@@ -136,7 +136,7 @@ $all_supers = mysqli_query($con, $sql);
                     <?php
 
                     // Connect to database
-                    $con = mysqli_connect("localhost", "fit3048", "fit3048", "fit3048_advice");
+                    $con = mysqli_connect("localhost", "u22s1018_fit3048", "#Ra7C1fW8Z68", "u22s1018_fit3048_advice_cyborg");
 
                     // mysqli_connect("servername","username","password","database_name")
 

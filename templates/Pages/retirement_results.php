4c984dcb3d5c1e582a1eec8dbc10e69cce27047f
hkil0001 <hkil0001@student.monash.edu>
2022-09-19 14:13:40 +1000

    Styling changes for Continue your retirement journey section links

--- templates/Pages/retirement_results.php
+++ templates/Pages/retirement_results.php
@@ -79,10 +79,10 @@ $_SESSION["age"] = $myAge;
             </div>
             <div class="row" style="margin-bottom:0.5em">
 <!--                <button type="button" onclick="location.href='risk_profile' " class="btn btn-primary" style="font-size: 2vh; border-radius: 12px; text-transform: uppercase">Investment portfolio</button>-->
-                <button style="width:35%;" type="button" onclick="location.href='#' " class="btn btn-primary" style="font-size: 2vh; border-radius: 12px; text-transform: uppercase">Investment portfolio(Coming Soon)</button>
+                <button style="width:35%;" type="button" onclick="location.href='#' " class="btn btn-primary" style="font-size: 2vh; border-radius: 12px; text-transform: uppercase">Investment portfolio (Coming Soon)</button>
             </div>
             <div class="row" style="margin-bottom:0.5em">
-                <button style="width:35%;" type="button" onclick="location.href='#' " class="btn btn-primary" style="font-size: 2vh; border-radius: 12px; text-transform: uppercase">Consolidate Super(Coming Soon)</button>
+                <button style="width:35%;" type="button" onclick="location.href='#' " class="btn btn-primary" style="font-size: 2vh; border-radius: 12px; text-transform: uppercase">Consolidate Super (Coming Soon)</button>
             </div>
             <div class="row" style="margin-bottom:0.5em">
                 <button style="width:35%;" type="button" onclick="location.href='income_stream' " class="btn btn-primary" style="font-size: 2vh; border-radius: 12px; text-transform: uppercase">Set up an Income Stream</button>
